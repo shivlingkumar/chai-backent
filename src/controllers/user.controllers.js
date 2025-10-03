8aad352903ec7274f85cda897c4ea87ee33e4d77
shivling kumar <shivlingkumar295@gmail.com>
2025-10-03 15:43:04 +0530

    incomingRefreshtoken bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -187,7 +187,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken =
       req.cookie.refreshToken || req.body.refreshToken;
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
       throw new ApiError(401, "Unauthorized request");
     }
     const decodedToken = jwt.verify(
